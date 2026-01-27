89cb8627a58261b0b369910317b0c6ed11eb57f0
H0llyW00dzZ <h0llyw00dzz@pm.me>
2026-01-27 23:40:10 +0700

    Bump Version
    - [+] chore(constants): bump SDK version to 0.2.7

--- src/constants/version.go
+++ src/constants/version.go
@@ -25,7 +25,7 @@ const (
 
 	// SDKVersion is the current version of this SDK.
 	// This should be updated with each release.
-	SDKVersion = "0.2.6"
+	SDKVersion = "0.2.7"
 )
 
 // SDKRepository is the GitHub repository URL for this SDK.
